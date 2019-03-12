8fe4bd292f6d4acd4557d4d8f03572ec394dd71f
Anthony <anthonylimac@gmail.com>
2019-03-12 11:12:48 -0300

    Original Data with Raw Data

---
+++ weedev-api/src/main/java/com/weeddevs/int/weedevapi/WeedevApiApplication.java
@@ -0,0 +1,13 @@
+package com.weeddevs.int.weedevapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeedevApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeedevApiApplication.class, args);
+	}
+
+}
---
+++ weedev-api/src/test/java/com/weeddevs/int/weedevapi/WeedevApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.weeddevs.int.weedevapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeedevApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
